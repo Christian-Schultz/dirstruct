51345086b4801abd844e7ddc1c352427e83f683e
Christian Schultz <schultz.christian@gmail.com>
2017-09-07 21:47:55 +0200

    Added core functionality

--- dirstruct/main.py
+++ dirstruct/main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+__version__ = 0.1
 
 description = 'Create a directory structure from file. Directories in the definition file should be separated by ' \
               'newlines and should follow the UNIX path convention. '
@@ -13,18 +14,46 @@ epilog = "Example:" \
 
 call_path = os.getcwd()
 
-parser = argparse.ArgumentParser(description=description, epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
+parser = argparse.ArgumentParser(prog='dirstruct', description=description, epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('dirfile',  type=str, help='the file containing the directory structure')
 parser.add_argument('--path', '-p', dest='path', action='store', default=call_path,
                     help='the root path where the directory structure should be created. Default: Current directory')
 parser.add_argument('--verbose', '-v', action='store_true', help='verbose mode', default=False)
 parser.add_argument('--debug', action='store_true', help='print debug information', default=False)
+parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=__version__))
+parser.add_argument('--relative', action='store_true', help='specifies whether the paths in the input file are '
+                                                            'relative or absolute paths. Default true', default=True)
+parser.add_argument('--remove', action='store_true', help='Inverse - remove directories instead of creating them. '
+                                                          'Will not remove recursively. Default false', default=False)
+
 
 args = parser.parse_args()
 
-if args.debug:
-    print(call_path)
+dirfile = os.path.realpath(args.dirfile)
 
-    print(parser.parse_args())
+if args.debug:
+    print("call_path: %s" % call_path)
+    print("dir_file: %s" % dirfile)
+    print("parser: %s " % parser.parse_args())
+
+dirs = []
+with open(dirfile, 'r') as f:
+    for line in f:
+        d = line.strip()
+        if args.relative:
+            d = os.path.realpath(call_path + os.sep + d)
+        if not args.remove:
+            if os.path.exists(d):
+                if args.verbose:
+                    "%s already exists, skipping" % d
+                continue
+            if args.verbose:
+                print("mkdir %s" % d)
+
+                    #os.makedirs(d)
+        else:
+            if args.verbose:
+                print("rm -r %d" % d)
+            #os.rmdir(d)
 
 print("Done")
