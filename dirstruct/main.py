fb7cd85188d695a3008b597e21160ee1ba65e22a
Christian Schultz <schultz.christian@gmail.com>
2017-09-07 22:44:09 +0200

    Changed --path to --root

--- dirstruct/main.py
+++ dirstruct/main.py
@@ -16,14 +16,12 @@ call_path = os.getcwd()
 
 parser = argparse.ArgumentParser(prog='dirstruct', description=description, epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('dirfile',  type=str, help='the file containing the directory structure.')
-parser.add_argument('--path', '-p', dest='path', action='store', default=call_path,
+parser.add_argument('--root', '-r', dest='root', action='store', default=call_path,
                     help='the root path where the directory structure should be created. Default: Current directory.')
 parser.add_argument('--verbose', '-v', action='store_true', help='verbose mode', default=False)
 parser.add_argument('--debug', action='store_true', help='print debug information. Will also toggle verbose mode.',
                     default=False)
 parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=__version__))
-parser.add_argument('--relative', action='store_true', help='specifies whether the paths in the input file are '
-                                                            'relative or absolute paths. Default true.', default=True)
 parser.add_argument('--remove', action='store_true', help='Inverse - remove directories instead of creating them. '
                                                           'Will not remove recursively. Default false.', default=False)
 
@@ -40,11 +38,15 @@ if args.debug:
     print("parser: %s " % parser.parse_args())
 
 dirs = []
+if not os.path.exists(args.root):
+    os.mkdir(args.root)
+
+os.chdir(args.root)
+
 with open(dirfile, 'r') as f:
     for line in f:
         d = line.strip()
-        if args.relative:
-            d = os.path.realpath(call_path + os.sep + d)
+
         if not args.remove:
             if os.path.exists(d):
                 if verbose:
